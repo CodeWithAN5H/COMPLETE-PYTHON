5111bc5cba250bd9bb5b8a34063bea7825557464
Ansh Singh <anshsingh411k@gmail.com>
2025-09-23 16:45:44 +0530

    added new files and renamed files

---
+++ 12-conditional expressions/if-else.py
@@ -0,0 +1,5 @@
+# X if condition else Y
+
+user_role = "guest"
+
+print("Full Access" if user_role == "admin" else "Limited Access")
---
+++ 13-String Methods/main.py
@@ -0,0 +1,26 @@
+# name = input("Enter your name: ")
+# phone_no = input("Enter your phone number: ")
+
+# result = len(name)   # returns length of string in int
+
+# result = name.find("n")    # for first occurrence
+
+# result = name.rfind("n")   # for last occurrence  (if no results both will result -1)   "r" --> reverse
+
+# name = name.capitalize()   # capitals first letter of string
+
+# name = name.upper()          # capitals all letters
+
+# name = name.lower()          # lowers all letters
+
+# result = name.isdigit()        # isdigit --> only digits "true", alphabets + digits "false", only alpha "false"
+
+# result = name.isalpha()         # isalpha --> same as isdigit but for alphabets, note --> " " space is not alpha
+
+# result = phone_no.count("-")        #counts no of "-" sign
+
+# phone_no = phone_no.replace("-", " ")  # to replace old "-" to new " "
+
+print(help(str))
+
+# print(phone_no)
