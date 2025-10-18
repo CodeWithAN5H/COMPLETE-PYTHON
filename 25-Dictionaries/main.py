97833d932aae4efc940d353533052cf50a2b03c8
Ansh Singh <anshsingh411k@gmail.com>
2025-10-18 19:49:17 +0530

    added new files

---
+++ 25-Dictionaries/main.py
@@ -0,0 +1,38 @@
+# dictionary = a collection of {key:value} pairs
+#             ordered and changeable. No dublicates
+
+capitals = {"USA": "Washington D.C.",
+            "India": "New Delhi",
+            "China": "Beijing",
+            "Russia": "Moscow"}
+
+# print(dir(capitals))
+# print(help(capitals))
+# print(capitals.get("Japan"))    # gives value associated with the mentioned key
+
+# if capitals.get("Russia"):
+#     print("That capital exists")
+# else:
+#     print("That capital doesn't exist")
+
+# capitals.update({"Germany": "Berlin"})
+# capitals.update({"USA": "Detroit"})
+# capitals.pop("China")
+# capitals.popitem()
+# capitals.clear()
+
+# keys = capitals.keys()
+# for key in capitals.keys():
+#     print(key)
+
+# values = capitals.values()
+# for value in capitals.values():
+#     print(value)
+
+# items = capitals.items()
+# print(items)
+# for key, value in items:
+#     print(f"{key}: {value}")
+
+
+# print(capitals)
---
+++ 27-RANDOM NUMBERS/main.py
@@ -0,0 +1,11 @@
+import random
+
+low = 1
+high = 100
+options = ("rock", "paper", "scissor")
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+# number = random.randint(low, high)
+# number = random.random()   #floating
+# option = random.choice(options)
+# random.shuffle(cards)
