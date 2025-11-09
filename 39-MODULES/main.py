698aa2bdeb4c5f395cb30353e050379cd4870269
Ansh Singh <anshsingh411k@gmail.com>
2025-11-09 16:32:04 +0530

    new files

---
+++ 39-MODULES/main.py
@@ -0,0 +1,21 @@
+# module = a file containing code you want to include in your program use 'import' to include a module (built-in or your own)
+#          useful to break up a large program reusable separate files.
+import math
+# print(help("modules")) #for searching available modules
+# print(help("math"))
+
+# import math
+# import math as m
+# from math import e
+
+# USE LECTURE FOR MORE DETAIL
+
+import example
+
+# result = example.pi
+# result = example.square(3)
+# result = example.cube(3)
+# result = example.circumference(3)
+# result = example.area(3)
+
+# print(result)
